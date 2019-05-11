bc23815da23d871ed9f7f180c25023d31eed21a5
fatimaqudsia <fatima.qudsia@gmail.com>
2019-05-11 06:17:49 +0500

    good

---
+++ mycvfinal.py
@@ -0,0 +1,67 @@
+#print bio-data:
+
+name= "Fatima Qudsia"
+age = "30"
+dob = "18/10/1988"
+address = "Rizwan socity karachi "
+email = "Fatima.qudsia@gmail.com"
+F_OF_I = "Wireless Communication ,Artificial Intelligence"
+gradu = "B.E from Iqra University"
+gradu_year = "2013"
+gradu2 = "B.Ed from Allama Iqabal University"
+gradu2_year = "2016"
+inter = "Intermediate (Pre-Engineering) from Khursheed Govt Girls Degree College Karachi"
+inter_year = "2006-2008"
+matric = "Matriculation (Science) from PAF School"
+matric_year = "2005"
+print(name)
+print("     PERSONAL")
+print("Father Name")
+print("Muhammad Ayub")
+print("Date of Birth")
+print(dob)
+print("     CONTACT")
+print(address)
+print(email)
+print("     FIELD OF INTEREST")
+print (F_OF_I)
+print("     OBJECTIVE: ")
+print("To groom my career in a dynamic environment of organization that capitalizes on my capabilities and provides opportunity for career development")
+print ("                    SCHOLASTIC")
+print("     Graduation")
+print(gradu)
+print(gradu_year)
+print("CGPA 3.1")
+print(gradu2)
+print(gradu2_year)
+print("Persentage 87")
+print("     Intermediate")
+print(inter)
+print(inter_year)
+print ("Grade c")
+print("     Matriculation ")
+print(matric)
+print(matric_year)
+print ("Grade B")
+print("                       REPORTS & PROJECTS")
+print("•    Final Year Project based on “Abnormality detection of Ecg signal using Artificial Intelligence")
+print("•	2-32v Regulated/5-18v fixed PCB Power Supply")
+print("•	PCB Project base “INTEL 8086” Motherboard with 4.77 MHz of frequency")
+print("•	Conveyor Belt")
+print("•	AM / FM – Transmitter and Receiver")
+print("•	Function Generator")
+print("•	Digital Logic Converters")
+print("                       SKILLS AND INTERESTS")
+print("•	Proficient in MS Office, MS Access")
+print("•	Matlab Proficient")
+print("•	C++  Proficient")
+print("•	HFSS Intermediate")
+print("•	Computer skill")
+print("•	Internet skill")
+print("•	Verbal Communication Skills")
+print("•	Written skills")
+print("•	Windows 2007 /XP")
+print("•	Team Player with proficient communication, coordination and leadership skills")
+print("                             End")
+
+
